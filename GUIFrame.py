f92df9042c7c302c6df2e1c5aa5891f5c1221f44
Tegan <tegan.straley@ucdenver.edu>
2017-03-21 15:45:06 -0600

    working about/export

--- GUIFrame.py
+++ GUIFrame.py
@@ -80,27 +80,31 @@ if __name__ == '__main__':
 
     root.title("KATTZraper");
     #root.geometry('500x500')
+    root.resizable(width=FALSE, height=FALSE)
 
     menu = Menu(root)
     root.config(menu=menu)  # what's under 'File' menu item
+    KATZmenu = Menu(menu)
+    menu.add_cascade(label="Zyber Katz", menu=KATZmenu)
+    KATZmenu.add_command(label="About KATTZ", command=GUIFunctions.About)
+
     fileMenu = Menu(menu)
     menu.add_cascade(label="File", menu=fileMenu)
     fileMenu.add_command(label="New Search", command=GUIFunctions.NewSearch)
     fileMenu.add_separator()
     fileMenu.add_command(label="Exit", command=root.quit)
 
-    aboutMenu = Menu(menu)  # what's under 'About' menu item
-    menu.add_cascade(label="About", menu=aboutMenu)
-    aboutMenu.add_command(label="Author")
-    aboutMenu.add_command(label="Zyber Katz")
+    editMenu = Menu(menu)  # what's under 'About' menu item
+    menu.add_cascade(label="Edit", menu=editMenu)
+    editMenu.add_command(label="edit")
+
 
     bar = TabBar(root, "Search")
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     tab1 = Tab(root, "Search")  # notice how this one's master is the root instead of the bar
-    #frame1 = Frame(tab1)
     label = Label(tab1, text="K.A.T.T.Z. web scraper").grid(row=0, column= 0, padx = 10, pady=10)
     jobTitleEntry = Label(tab1, text="Job title").grid(row=1, sticky=W, padx = 10, pady=10)
-    entry1 = Entry(tab1).grid(row=1, column=1, padx = 10, pady=10)
+    entry1 = Entry(tab1, text = "").grid(row=1, column=1, padx = 10, pady=10)
     stateEntry = Label(tab1, text="State").grid(row=2, sticky=W, padx = 10, pady=10)
     entry2 = Entry(tab1).grid(row=2, column=1,padx = 10, pady=10)
     CityEntry = Label(tab1, text="City").grid(row=3, sticky=W, padx = 10, pady=10)
@@ -112,7 +116,7 @@ if __name__ == '__main__':
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     tab2 = Tab(root, "Results")
-    title = Label(tab2, text="Choose a search").grid(row=0, column=0, padx=10, pady=10)
+    title = Label(tab2, text="Choose a job title").grid(row=0, column=0, padx=10, pady=10)
     savedSearch = StringVar(tab2)
     savedSearch.set("search") #default saved search
     savedSearchOption = OptionMenu(tab2, savedSearch, "search", "two")
@@ -122,7 +126,7 @@ if __name__ == '__main__':
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     tab3 = Tab(root, "Analytics")
-    title = Label(tab3, text="Choose a search").grid(row=0, column=0, padx=10, pady=10)
+    title = Label(tab3, text="Choose a job title").grid(row=0, column=0, padx=10, pady=10)
     savedSearch = StringVar(tab3)
     savedSearch.set("search") #default saved search
     savedSearchOption = OptionMenu(tab3, savedSearch, "search", "two")
@@ -133,39 +137,18 @@ if __name__ == '__main__':
     tab4 = Tab(root, "Export/Import")
     #Option menu for saved search
     savedSearch = StringVar(tab4)
-    savedSearch.set("choose a saved search") #default saved search
-    savedSearchOption = OptionMenu(tab4, savedSearch, "choose a saved search", "two")
-    savedSearchOption.grid(row=0, column= 0, padx = 5, pady=5)
+    savedSearch.set("Choose a Job title") #default saved search
+    savedSearchOption = OptionMenu(tab4, savedSearch, "Choose a Job title", "software dev", "ER doctor")
+    savedSearchOption.pack(padx = 5, pady=5)
 
-    #
-    renameSearch = Label(tab4, text="Rename Search: ").grid(row=1, sticky=W, padx = 5, pady=5)
-    entry1 = Entry(tab4).grid(row=1, column=1, padx = 5, pady=5)
-    searchExtension = StringVar(tab4)
-    searchExtension.set("choose a file extension") #default
-    searchExtensionOption = OptionMenu(tab4, searchExtension, "choose a file extension", ".pdf", ".jpeg", ".?others?")
-    searchExtensionOption.grid(row=1, column= 2, padx = 5, pady=5)
-    exportDestination = Label(tab4, text="Export Destination").grid(row=2, sticky=W, padx = 5, pady=5)
-    entry2 = Entry(tab4).grid(row=2, column=1,padx = 5, pady=5)
-    entry2Button = Button(tab4, text="Choose location").grid(row=2, column=2,padx = 5, pady=5, command=GUIFunctions.chooseExportLocation(tab4, entry2))
-    exportButton = Button(tab4, text="Export").grid(row=2, column=3, padx=5, pady=5 ) #command= export function garrett writes)
-
-    importDestination = Label(tab4, text="Import Destination").grid(row=3, sticky=W, padx = 5, pady=5)
-    entry3 = Entry(tab4).grid(row=3, column=1, padx = 5, pady=5)
-    entry3Button = Button(tab4, text="Choose location").grid(row=3, column=2,padx = 5, pady=5, command=GUIFunctions.chooseExportLocation(tab4, entry3))
-    importButton = Button(tab4, text="Import").grid(row=3, column=3, padx=5, pady=5 ) #command= import function garrett writes)
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    tab5 = Tab(root, "About")
-    authors = Label(tab5, text="Authors:").grid(row=1, column=0, padx=5, pady=5)
-    authorsNames = Label(tab5, text="Justin, G, Garrett, Tegan").grid(row=1, column=1, padx=5, pady=5)
-    purpose = Label(tab5, text="Purpose:").grid(row=2, column=0, padx=5, pady=5)
-    purposeText = Label(tab5, text="Indeed.com scrapper to learn which\n qualifications are looked for in jobs.").grid(row=2, column=1, padx=5, pady=5)
+    entry2Button = Button(tab4, text="Export destination", command=GUIFunctions.output).pack(padx = 5, pady=5)
+    entry3Button = Button(tab4, text="Import search", command=GUIFunctions.output).pack(padx = 5, pady=5)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     bar.add(tab1)  # add the tabs to the tab bar
     bar.add(tab2)
     bar.add(tab3)
     bar.add(tab4)
-    bar.add(tab5)
 
     bar.config(bd=2, relief=RIDGE)			# add some border
 
