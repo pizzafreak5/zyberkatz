447c06567c8152b878bebc1d6225541bd099198c
Garrett <pf5dev@gmail.com>
2017-04-15 14:14:37 -0600

    scraper works more

--- test_search.py
+++ test_search.py
@@ -2,25 +2,20 @@ import search
 import sqlite3
 import db
 
-search_options = {
-    'website_url' : 'http://www.indeed.com/jobs?q=Computer+Science&l=Denver%2C+CO',
-    'div_class_links':'pagination',     
-    'div_class_listings':['  row  result', 'row  result',  'lastRow  row  result', 'row result', ' row result', ' lastRow row result', 'lastRow row result'],
-    'job_type':'',
-    'salary_est':'',
-    'company_name_tag':'span',
-    'company_name_attrs':{'class':'company'},
-    'time_posted_tag':'span',
-    'time_posted_attrs':{'class':'date'},
-    'job_loc_tag':'span',
-    'job_loc_attrs':{'class':'location'},
-    'job_desc_tag':'span',
-    'job_desc_attrs':{'class':'summary'},
+search_options = {'search_title':'something',
+                       'search_value':'COMPUTER SCIENC',
+                       'job_location':'DENVER, CO',
+                       'job_salary_est':'',
+                       'limit':'',
+                       'job_type':'',
+                       'job_experience':'',
+                       'website_url':'',
+                       'global_listings_table':'listing',
+                       'global_search_table':'search',
+                       'global_search_junction': 'search_junction',
+                       'global_db_name':'zyber.db'}
 
-    'job_title_tag':'a'
-    }    
 
-'''    
 search_obj = search.Search(search_options)
 search_obj.search()
 
@@ -45,11 +40,10 @@ column_names = ['Hash value',
                 'Job page text'
                 ]
 
-for row in db_cursor.execute('SELECT * FROM temporary'):
+for row in db_cursor.execute('SELECT * FROM search'):
     print ('ENTRY:\n**********************************************************')
     for i in range(len(row)):
-        print (column_names[i] + ':' + row[i])
+        print (row[i])
     print('**********************************************************\n')
 
 
-'''
