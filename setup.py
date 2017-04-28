611d5c65eb87cf7f9b54abc63c647297fa54342a
Gediminas Jakstonis <Gediminas Jakstonis>
2017-04-28 14:54:10 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='KATTZscraper',
+      version='1.0.4',
+      description='Python web scraper for indeed.com',
+      author='Justin Hockenberry, Gediminas Jakstonis, Garrett Schwartz, Tegan Straley',
+      url='https://github.com/pizzafreak5/zyberkatz',
+      install_requires=['beautifulsoup4', 'matplotlib', 'numpy', 'lxml', 'pandas', 'requests'],
+      )
